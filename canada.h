0d357903ee8157b8a168f3a96a346aa80a4e7d7d
afsheenm <123511034+afsheenm@users.noreply.github.com>
2023-03-07 22:42:55 +0000

    Code by the end of Week 8 lesson

--- canada.h
+++ canada.h
@@ -2,7 +2,13 @@
 #define CANADA_H
 #include <stdio.h>
 #include <string.h>
+#include "canada.h"
 
+void print_provinces()
+{
+    //TODO:: Write a for loop that iterates over the PROVINCES array and prints out how many letters is contained in each
+
+}
 
 
 #endif
